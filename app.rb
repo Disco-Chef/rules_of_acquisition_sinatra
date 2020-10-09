f53890e078b1cee613ce813e2f9d0772dcf8d05a
Nikos Agathos <nicolas.agathos@gmail.com>
2020-10-09 19:00:08 +0200

    works as of now, need to return json for conformity and to make it be a real boy

--- app.rb
+++ app.rb
@@ -1,24 +1,16 @@
 require 'sinatra'
 require 'json'
 
-get '/' do
-  'Hello world!'
+get '/api/v1/rules/random' do
+  rule_number = (1..286).to_a.sample.to_s
+  serialized_rules = File.read("rules_of_acquisition.json")
+  rules = JSON.parse(serialized_rules)
+  "#{rule_number}: #{rules[rule_number]}"
 end
 
 get '/api/v1/rules/:rule_number' do
-  rule_number = params[:rule_number].to_s
+  rule_number = params[:rule_number]
   serialized_rules = File.read("rules_of_acquisition.json")
   rules = JSON.parse(serialized_rules)
-  {
-    rule_number => rules[rule_number]
-  }
-end
-
-get '/api/v1/rules/' do
-  rule_number = params[:rule_number].to_s
-  serialized_rules = File.read("rules_of_acquisition.json")
-  rules = JSON.parse(serialized_rules)
-  {
-    rule_number => rules[rule_number]
-  }
+  "#{rule_number}: #{rules[rule_number]}"
 end
