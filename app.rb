58c78987f35416c5aef8042293a8486dbab7800d
Nikos Agathos <nicolas.agathos@gmail.com>
2020-10-09 18:43:47 +0200

    typos and route for specific rule

--- app.rb
+++ app.rb
@@ -9,5 +9,16 @@ get '/api/v1/rules/:rule_number' do
   rule_number = params[:rule_number].to_s
   serialized_rules = File.read("rules_of_acquisition.json")
   rules = JSON.parse(serialized_rules)
-  rules[rule_number]
+  {
+    rule_number => rules[rule_number]
+  }
+end
+
+get '/api/v1/rules/' do
+  rule_number = params[:rule_number].to_s
+  serialized_rules = File.read("rules_of_acquisition.json")
+  rules = JSON.parse(serialized_rules)
+  {
+    rule_number => rules[rule_number]
+  }
 end
