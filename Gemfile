a6607067157811e3e15e26823959c0f7b8d0f297
Nikos Agathos <nicolas.agathos@gmail.com>
2020-10-17 04:21:32 +0200

    well, that's something

--- Gemfile
+++ Gemfile
@@ -2,5 +2,10 @@ source 'https://rubygems.org'
 
 ruby '2.6.6'
 
-gem 'sinatra'
-gem 'json'
+gem "sinatra"
+gem "sinatra-contrib"
+gem "pry-byebug"
+gem "better_errors"
+gem "binding_of_caller"
+gem "sinatra-static-assets"
+gem 'html'
--- app.rb
+++ app.rb
@@ -1,17 +1,61 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+require 'json'
+require 'html'
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+before do
+  content_type :json
+end
+
+
 get '/' do
-  'Sinatra sees your attempt and raises you 100'
+  # link_to "Rules of Acquisition Index", "/api/v1/rules"
+  # link_to "Random Rule of Acquisition", "/api/v1/rules/random"
+  # link_to "Specific Rule of Acquisition", "/api/v1/rules/1"
+  # "(change 1 to anything up to 286)"
 end
 
 get '/api/v1/rules/random' do
   rule_number = (1..286).to_a.sample.to_s
-  serialized_rules = File.read("rules_of_acquisition.json")
+  serialized_rules = File.read("data/rules_of_acquisition.json")
   rules = JSON.parse(serialized_rules)
-  "#{rule_number}: #{rules[rule_number]}"
+  {
+    success: true,
+    message: "Successfully got Rule of Acquisition",
+    data: {
+      rule_number: rule_number,
+      scripture: rules[rule_number]
+    }
+  }.to_json
 end
 
 get '/api/v1/rules/:rule_number' do
   rule_number = params[:rule_number]
-  serialized_rules = File.read("rules_of_acquisition.json")
+  serialized_rules = File.read("data/rules_of_acquisition.json")
   rules = JSON.parse(serialized_rules)
-  "#{rule_number}: #{rules[rule_number]}"
+  {
+    success: true,
+    message: "Successfully got Rule of Acquisition",
+    data: {
+      rule_number: rule_number,
+      scripture: rules[rule_number]
+    }
+  }.to_json
+end
+
+get '/api/v1/rules' do
+  serialized_rules = File.read("data/rules_of_acquisition.json")
+  rules = JSON.parse(serialized_rules)
+  {
+    success: true,
+    message: "Successfully got Rule of Acquisition",
+    data: rules
+  }.to_json
 end
